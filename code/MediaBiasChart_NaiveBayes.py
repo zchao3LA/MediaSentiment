42ee11e67ebe0a8ac28b22495d44f20315546797
zchao3LA <46730058+zchao3LA@users.noreply.github.com>
2020-05-10 15:35:01 -0700

    NB test

---
+++ code/MediaBiasChart_NaiveBayes.py
@@ -0,0 +1,78 @@
+import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer
+import matplotlib.pyplot as plt
+import numpy as np
+from CleanData import clean_comment, get_bag_of_words
+
+
+extreme_frac = 0.2  # This extreme_frac stands for the percentage of media to be selected as left/right, high/low media
+
+
+
+
+# Load all article reviews from MediaBiasChart V5.0:
+media_bias = pd.read_csv('../data/MediaBiasChart.csv')
+media_bias = media_bias.groupby('Source').mean()
+media_bias.reset_index(level=0, inplace=True)
+
+MediaBiasChart_to_Tweet = {'ABC':'ABC','AP':'AP','Axios':'axios','CNN':'CNN','Wall Street Journal':'WSJ',\
+    'The Atlantic':'TheAtlantic','The Hill':'thehill', 'BBC':'BBC', 'Think Progress':'thinkprogress',\
+    'MSNBC':'MSNBC','The Nation':'thenation','Daily Beast':'thedailybeast','Mother Jones':'MotherJones',\
+    'CNSNews':'cnsnews','Fox News':'FoxNews', 'The Federalist':'FDRLST','Breitbart':'BreitbartNews',\
+    'Daily Caller':'DailyCaller','The Blaze':'theblaze','Business Insider':'businessinsider',\
+    'CBS':'CBSNews','The Economist':'TheEconomist','BuzzFeed':'BuzzFeed','Daily Signal':'DailySignal',\
+    'New Republic':'newrepublic','Foreign Policy':'ForeignPolicy','IJR':'TheIJR','National Review':'NRO',\
+    'National Public Radio':'NPR','New York Post':'nypost','New York Times':'nytimes','The New Yorker':'NewYorker',\
+    'NewsMax':'newsmax','One America News Network':'OANN','Politico':'politico','Quartz':'qz',\
+    'Reason':'reason','Reuters':'Reuters','Slate':'Slate','Talking Points Memo':'TPM','Vanity Fair':'VanityFair',\
+    'Vox':'voxdotcom','Washington Examiner':'dcexaminer','Washington Free Beacon':'FreeBeacon',\
+    'Washington Post':'washingtonpost','Washington Times':'WashTimes','The Week':'TheWeek','Bloomberg':'Bloomberg',\
+    'Christian Science Monitor':'csmonitor', 'Democracy Now':'democracynow','Financial Times':'FT',\
+    'Fiscal Times':'TheFiscalTimes','Forbes':'Forbes','Fortune':'FortuneMagazine','Forward':'jdforward',\
+    'FreeSpeech TV':'freespeechtv','Huffington Post':'HuffPost','LA Times':'latimes','Marketwatch':'MarketWatch',\
+    'OZY':'ozy','PBS':'PBS','ProPublica':'propublica','Time':'TIME','USA Today':'USATODAY',\
+    'Weather.com':'weatherchannel'}
+
+media_bias['Source'] = media_bias.Source.map(MediaBiasChart_to_Tweet)
+media_bias = media_bias.dropna()
+media_bias = media_bias.reset_index(drop=True)
+
+left_bound = media_bias.Bias.quantile(extreme_frac)
+right_bound = media_bias.Bias.quantile(1-extreme_frac)
+low_bound = media_bias.Quality.quantile(extreme_frac)
+high_bound = media_bias.Quality.quantile(1-extreme_frac)
+all_media = media_bias['Source'].tolist()
+left_media = media_bias.loc[media_bias['Bias']<=left_bound]
+left_media = left_media['Source'].tolist()
+right_media = media_bias.loc[media_bias['Bias']>=right_bound]
+right_media = right_media['Source'].tolist()
+low_media = media_bias.loc[media_bias['Quality']<=low_bound]
+low_media = low_media['Source'].tolist()
+high_media = media_bias.loc[media_bias['Quality']>=high_bound]
+high_media = high_media['Source'].tolist()
+
+
+df1 = pd.read_csv('../data/filtered_part1.csv', skiprows=1)
+df2 = pd.read_csv('../data/filtered_part2.csv')
+df3 = pd.read_csv('../data/filtered_part3.csv')
+df4 = pd.read_csv('../data/filtered_part4.csv',lineterminator='\n')
+df6 = pd.read_csv('../data/filtered_part6.csv')
+df7 = pd.read_csv('../data/filtered_part7.csv')
+df8 = pd.read_csv('../data/filtered_part8.csv')
+df = pd.concat([df1, df2, df3, df4, df6, df7, df8], sort = False)
+
+all_media = media_bias['Source'].tolist()
+df = df.loc[df['user_screen_name'].isin(all_media)]
+
+print('Total number of tweets: ')
+print(df.shape[0])
+
+df_2018 = df.loc[((df['created_at']) >= '2018-01-01') & ((df['created_at']) <= '2018-12-31')]
+df_2018 = df_2018.reset_index(drop = True)
+import preprocessor as p
+df_2018['text']  = df_2018['text'].apply(p.clean)
+
+bag_of_words, vectorizer = get_bag_of_words(df_2018['text'],ngram_range=(1,3), min_df=0.0002)
+
+print('Shape of bag_of_words: ')
+print(bag_of_words.shape)
